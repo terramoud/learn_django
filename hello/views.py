252d635b9ba67b88ec2d34e86f57a3fc558a8065
Oleksandr Koreshev <a.koreshev1@gmail.com>
2023-12-29 21:35:06 +0200

    create routes

--- hello/views.py
+++ hello/views.py
@@ -1,8 +1,16 @@
-from django.http import HttpResponse
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
 
 # Create your views here.
 
-def index(request):
+def index(request: HttpRequest):
     return HttpResponse("Hello, world. You're at the hello index.")
+
+
+def some(request: HttpRequest):
+    return HttpResponse("some text")
+
+
+def greet(request: HttpRequest, name: str):
+    return HttpResponse(f"Hello, {name.capitalize()}!")
